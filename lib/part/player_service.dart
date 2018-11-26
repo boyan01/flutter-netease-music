8f75aacd3e17de1fd57e3cf40906414364558df3
boyan <yangbinyhbn@gmail.com>
2018-11-26 23:12:29 +0800

    upgrade state update architecture

--- lib/part/player_service.dart
+++ lib/part/player_service.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/services.dart';
+import 'package:flutter/widgets.dart';
+import 'package:meta/meta.dart';
 import 'package:quiet/model/model.dart';
 
 const MethodChannel _channel = const MethodChannel('tech.soit.quiet/player');
@@ -56,9 +58,7 @@ class MusicPlayer {
     //TODO
   }
 
-  void playPrevious() {
-
-  }
+  void playPrevious() {}
 
   Future<bool> setVolume(double volume) async {
     var success = await _channel.invokeMethod("volume", volume);
@@ -76,3 +76,91 @@ class MusicPlayer {
 }
 
 enum PlayerState { playing, buffering, pause, idle }
+
+class Quiet extends StatefulWidget {
+  Quiet(
+      {@Required() this.child,
+      this.playerState = false,
+      this.playingMusic = false});
+
+  ///listen player state event
+  ///use [MusicPlayerState.of]
+  final bool playerState;
+
+  ///listen playing music change event
+  final bool playingMusic;
+
+  final Widget child;
+
+  @override
+  State<StatefulWidget> createState() => _QuietState();
+}
+
+class _QuietState extends State<Quiet> {
+  PlayerState state;
+
+  Music current;
+
+  void _onMusicChange(Music music) {
+    setState(() {
+      current = music;
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    quiet.addMusicChangeListener(_onMusicChange);
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    quiet.removeMusicChangeListener(_onMusicChange);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    var result = widget.child;
+
+    if (widget.playerState) {
+      result = MusicPlayerState(result, state);
+    }
+    if (widget.playingMusic) {
+      result = PlayingMusic(result, current);
+    }
+    return result;
+  }
+}
+
+class MusicPlayerState extends InheritedWidget {
+  ///get current Music player state
+  static MusicPlayerState of(BuildContext context) {
+    return context.inheritFromWidgetOfExactType(MusicPlayerState);
+  }
+
+  MusicPlayerState(Widget child, this.state) : super(child: child);
+
+  final PlayerState state;
+
+  @override
+  bool updateShouldNotify(MusicPlayerState oldWidget) {
+    return state == oldWidget.state;
+  }
+}
+
+class PlayingMusic extends InheritedWidget {
+  PlayingMusic(Widget child, this.playing) : super(child: child);
+
+  ///get current playing music
+  final Music playing;
+
+  static PlayingMusic of(BuildContext context) {
+    return context.inheritFromWidgetOfExactType(PlayingMusic);
+  }
+
+  @override
+  bool updateShouldNotify(PlayingMusic oldWidget) {
+    return playing != oldWidget.playing;
+  }
+}
