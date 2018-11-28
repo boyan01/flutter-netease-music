4ad425474ebfb4fbddcb430fe5389f8daa9a0cbd
boyan01 <yangbinyhbn@gmail.com>
2018-11-28 15:12:05 +0800

    improve play_service and playing page

--- lib/pages/page_playing.dart
+++ lib/pages/page_playing.dart
@@ -3,6 +3,7 @@ import 'dart:ui';
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:quiet/part/part.dart';
+import 'package:video_player/video_player.dart';
 
 class PlayingPage extends StatelessWidget {
   @override
@@ -12,16 +13,19 @@ class PlayingPage extends StatelessWidget {
         child: Stack(
           children: <Widget>[
             _BlurBackground(),
-            Column(
-              children: <Widget>[
-                _PlayingTitle(),
-                Spacer(),
-                _AlbumCover(),
-                Spacer(),
-                _OperationBar(),
-                _DurationProgressBar(),
-                _ControllerBar(),
-              ],
+            Material(
+              color: Colors.transparent,
+              child: Column(
+                children: <Widget>[
+                  _PlayingTitle(),
+                  Spacer(),
+                  _AlbumCover(),
+                  Spacer(),
+                  _OperationBar(),
+                  _DurationProgressBar(),
+                  _ControllerBar(),
+                ],
+              ),
             ),
           ],
         ),
@@ -34,6 +38,40 @@ class _ControllerBar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var color = Theme.of(context).primaryIconTheme.color;
+    var state = PlayerState.of(context).value.state;
+
+    Widget iconPlayPause;
+    if (state.isPlaying) {
+      iconPlayPause = IconButton(
+          tooltip: "暂停",
+          iconSize: 40,
+          icon: Icon(
+            Icons.pause_circle_outline,
+            color: color,
+          ),
+          onPressed: () {
+            quiet.pause();
+          });
+    } else if (state.isBuffering) {
+      iconPlayPause = SizedBox(
+        height: 40,
+        width: 40,
+        child: Center(
+          child: CircularProgressIndicator(),
+        ),
+      );
+    } else {
+      iconPlayPause = IconButton(
+          tooltip: "播放",
+          iconSize: 40,
+          icon: Icon(
+            Icons.play_circle_outline,
+            color: color,
+          ),
+          onPressed: () {
+            quiet.play();
+          });
+    }
 
     return Container(
       padding: EdgeInsets.symmetric(vertical: 16),
@@ -52,15 +90,10 @@ class _ControllerBar extends StatelessWidget {
                 Icons.skip_previous,
                 color: color,
               ),
-              onPressed: () {}),
-          IconButton(
-              tooltip: "播放/暂停",
-              iconSize: 40,
-              icon: Icon(
-                Icons.play_circle_outline,
-                color: color,
-              ),
-              onPressed: () {}),
+              onPressed: () {
+                quiet.playPrevious();
+              }),
+          iconPlayPause,
           IconButton(
               tooltip: "下一曲",
               iconSize: 36,
@@ -68,7 +101,9 @@ class _ControllerBar extends StatelessWidget {
                 Icons.skip_next,
                 color: color,
               ),
-              onPressed: () {}),
+              onPressed: () {
+                quiet.playNext();
+              }),
           IconButton(
               tooltip: "当前播放列表",
               icon: Icon(
@@ -86,21 +121,62 @@ class _DurationProgressBar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var theme = Theme.of(context).primaryTextTheme;
+    var state = PlayerState.of(context).value.state;
+
+    Widget progressIndicator;
+
+    String durationText;
+    String positionText;
+
+    if (state.initialized) {
+      var duration = state.duration.inMilliseconds;
+      var position = state.position.inMilliseconds;
+
+      durationText = getTimeStamp(duration);
+      positionText = getTimeStamp(position);
+
+      int maxBuffering = 0;
+      for (DurationRange range in state.buffered) {
+        final int end = range.end.inMilliseconds;
+        if (end > maxBuffering) {
+          maxBuffering = end;
+        }
+      }
+
+      progressIndicator = Stack(
+        fit: StackFit.passthrough,
+        children: <Widget>[
+          LinearProgressIndicator(
+            value: maxBuffering / duration,
+            valueColor: AlwaysStoppedAnimation<Color>(Colors.white70),
+            backgroundColor: Colors.white12,
+          ),
+          LinearProgressIndicator(
+            value: position / duration,
+            valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
+            backgroundColor: Colors.transparent,
+          ),
+        ],
+      );
+    } else {
+      progressIndicator = LinearProgressIndicator(
+        value: null,
+        valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
+        backgroundColor: Colors.transparent,
+      );
+    }
+
     return Padding(
       padding: EdgeInsets.symmetric(vertical: 4, horizontal: 16),
       child: Row(
         children: <Widget>[
-          Text("00:00", style: theme.body2),
-          Padding(padding: EdgeInsets.only(left: 4)),
+          Text(positionText ?? "00:00", style: theme.body1),
+          Padding(padding: EdgeInsets.only(left: 8)),
           Expanded(
-            child: Slider(
-              value: 0,
-              activeColor: theme.body1.color,
-              onChanged: (v) {},
-            ),
+            child: progressIndicator,
           ),
-          Padding(padding: EdgeInsets.only(left: 4)),
-          Text("00:00", style: theme.body2),
+          Padding(padding: EdgeInsets.only(left: 8)),
+          Text(durationText ?? "00:00", style: theme.body1),
         ],
       ),
     );
