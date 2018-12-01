598d33fb2a42589c8b420303caff32387ed374c2
yangbin10 <yangbin10@jd.com>
2018-12-01 12:52:00 +0800

    add player state persistence

--- lib/model/model.dart
+++ lib/model/model.dart
@@ -29,6 +29,29 @@ class Music {
   String toString() {
     return 'Music{id: $id, title: $title, url: $url, album: $album, artist: $artist}';
   }
+
+  static Music fromMap(Map map) {
+    if (map == null) {
+      return null;
+    }
+    return Music(
+        id: map["id"],
+        title: map["title"],
+        url: map["url"],
+        album: Album.fromMap(map["album"]),
+        artist:
+            (map["artist"] as List).cast<Map>().map(Artist.fromMap).toList());
+  }
+
+  Map toMap() {
+    return {
+      "id": id,
+      "title": title,
+      "url": url,
+      "album": album.toMap(),
+      "artist": artist.map((e) => e.toMap()).toList()
+    };
+  }
 }
 
 class Album {
@@ -55,6 +78,15 @@ class Album {
   String toString() {
     return 'Album{name: $name, id: $id}';
   }
+
+  static Album fromMap(Map map) {
+    return Album(
+        id: map["id"], name: map["name"], coverImageUrl: map["coverImageUrl"]);
+  }
+
+  Map toMap() {
+    return {"id": id, "name": name, "coverImageUrl": coverImageUrl};
+  }
 }
 
 class Artist {
@@ -67,5 +99,13 @@ class Artist {
   @override
   String toString() {
     return 'Artist{name: $name, id: $id}';
+  }
+
+  static Artist fromMap(Map map) {
+    return Artist(id: map["id"], name: map["name"]);
+  }
+
+  Map toMap() {
+    return {"id": id, "name": name};
   }
 }
