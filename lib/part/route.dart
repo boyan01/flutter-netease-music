94da9bf30ab2ad2e2d9d2512e3787054311d70ac
yangbin10 <yangbin10@jd.com>
2018-12-01 14:46:32 +0800

    add leaderboard

--- lib/part/route.dart
+++ lib/part/route.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:quiet/pages/page_leaderboard.dart';
 import 'package:quiet/pages/page_login.dart';
 import 'package:quiet/pages/page_main.dart';
 import 'package:quiet/pages/page_playing.dart';
@@ -7,6 +8,8 @@ export 'package:quiet/pages/page_login.dart';
 export 'package:quiet/pages/page_main.dart';
 export 'package:quiet/pages/page_playlist_detail.dart';
 export 'package:quiet/pages/page_playing.dart';
+export 'package:quiet/pages/page_leaderboard.dart';
+
 
 const ROUTE_MAIN = "/";
 
@@ -16,9 +19,12 @@ const ROUTE_PLAYLIST_DETAIL = "/playlist/detail";
 
 const ROUTE_PAYING = "/playing";
 
+const ROUTE_LEADERBOARD = "/leaderboard";
+
 ///app routers
 final Map<String, WidgetBuilder> routes = {
   ROUTE_MAIN: (context) => MainPage(),
   ROUTE_LOGIN: (context) => LoginPage(),
   ROUTE_PAYING: (context) => PlayingPage(),
+  ROUTE_LEADERBOARD: (context) => LeaderboardPage()
 };
