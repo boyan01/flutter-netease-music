6740b9392c8b5db424b3e41c2044b9c43d931c9f
yangbin10 <yangbin10@jd.com>
2018-12-01 11:17:53 +0800

    ...

--- lib/part/part_player.dart
+++ lib/part/part_player.dart
@@ -1,83 +1,9 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/scheduler.dart';
 import 'package:quiet/part/route.dart';
-import 'package:quiet/repository/netease.dart';
 
 import 'player_service.dart';
 
-///登录状态
-///在上层嵌套 LoginStateWidget 以监听用户登录状态
-class LoginState extends InheritedWidget {
-  ///根据BuildContext获取 [LoginState]
-  static LoginState of(BuildContext context) {
-    return context.inheritFromWidgetOfExactType(LoginState);
-  }
-
-  LoginState(this.user, this.child) : super(child: child);
-
-  ///null -> not login
-  ///not null -> login
-  final Map<String, Object> user;
-
-  final Widget child;
-
-  @override
-  bool updateShouldNotify(LoginState oldWidget) {
-    var update = user != oldWidget.user;
-    return update;
-  }
-
-  ///当前是否已登录
-  bool get isLogin {
-    return user != null;
-  }
-
-  ///当前登录用户的id
-  int get userId {
-    if (!isLogin) {
-      throw Exception("当前没有用户登录");
-    }
-    Map<String, Object> account = user["account"];
-    return account["id"];
-  }
-}
-
-class LoginStateWidget extends StatefulWidget {
-  LoginStateWidget(this.child);
-
-  final Widget child;
-
-  @override
-  State<StatefulWidget> createState() => _LoginState();
-}
-
-class _LoginState extends State<LoginStateWidget> {
-  Map<String, Object> user;
-
-  @override
-  void initState() {
-    super.initState();
-    neteaseRepository.user.addListener(_onUserChanged);
-  }
-
-  void _onUserChanged() {
-    setState(() {
-      this.user = neteaseRepository.user.value;
-    });
-  }
-
-  @override
-  void dispose() {
-    super.dispose();
-    neteaseRepository.user.removeListener(_onUserChanged);
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return LoginState(user, widget.child);
-  }
-}
 
 class BoxWithBottomPlayerController extends StatelessWidget {
   BoxWithBottomPlayerController(this.child);
