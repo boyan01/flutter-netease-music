efe94b3a68e38d156b166cab25990cbe55117b7a
boyan01 <yangbinyhbn@gmail.com>
2018-12-06 14:39:58 +0800

    refactor image cache

--- lib/repository/netease_image.dart
+++ lib/repository/netease_image.dart
@@ -3,6 +3,7 @@ import 'dart:typed_data';
 
 import 'package:flutter/foundation.dart';
 import 'package:flutter/widgets.dart';
+import 'package:path_provider/path_provider.dart';
 import 'dart:ui' as ui show Codec;
 
 import 'package:quiet/part/part_cache.dart';
@@ -50,15 +51,13 @@ class NeteaseImage extends ImageProvider<NeteaseImage> implements CacheKey {
 
   Future<ui.Codec> _loadAsync(NeteaseImage key) async {
     assert(key == this);
-    var cache = await quietImageCache();
-    File image = await cache.getCacheFile(key);
-    if (image != null) {
-      debugPrint("cached hited : ${image.path}");
-      List<int> bytes = await image.readAsBytes();
-      return await PaintingBinding.instance
-          .instantiateImageCodec(Uint8List.fromList(bytes));
-    }
+    var cache = await _imageCache();
 
+    var image = await cache.get(key);
+    if (image != null) {
+      return PaintingBinding.instance
+          .instantiateImageCodec(Uint8List.fromList(image));
+    }
     //request network source
     final Uri resolved = Uri.base.resolve(key.url);
     final HttpClientRequest request = await _httpClient.getUrl(resolved);
@@ -75,12 +74,7 @@ class NeteaseImage extends ImageProvider<NeteaseImage> implements CacheKey {
       throw Exception('NetworkImage is an empty file: $resolved');
 
     //save image to cache
-    image = await cache.newCacheFile(key);
-    image = await image.writeAsBytes(bytes, flush: true);
-
-    assert((await image.length()) != 0,
-        " image $this saved to  ${image.path} with size = ${image.length()} ");
-
+    await cache.update(key, bytes);
 
     return await PaintingBinding.instance.instantiateImageCodec(bytes);
   }
@@ -98,5 +92,47 @@ class NeteaseImage extends ImageProvider<NeteaseImage> implements CacheKey {
   @override
   String getKey() {
     return id;
+  }
+}
+
+_ImageCache __imageCache;
+
+Future<_ImageCache> _imageCache() async {
+  if (_imageCache != null) {
+    return __imageCache;
+  }
+  var temp = await getTemporaryDirectory();
+  var dir = Directory(temp.path + "/quiet_images/");
+  if (!(await dir.exists())) {
+    dir = await dir.create();
+  }
+  __imageCache = _ImageCache(dir);
+  return __imageCache;
+}
+
+///cache netease image data
+class _ImageCache implements Cache<Uint8List> {
+  _ImageCache(Directory dir) : provider = FileCacheProvider(dir);
+
+  final FileCacheProvider provider;
+
+  @override
+  Future<Uint8List> get(CacheKey key) async {
+    var file = provider.getFile(key);
+    if (await file.exists()) {
+      return Uint8List.fromList(await file.readAsBytes());
+    }
+    return null;
+  }
+
+  @override
+  Future<bool> update(CacheKey key, Uint8List t) async {
+    var file = provider.getFile(key);
+    if (await file.exists()) {
+      file.delete();
+    }
+    file = await file.create();
+    await file.writeAsBytes(t);
+    return await file.exists();
   }
 }
