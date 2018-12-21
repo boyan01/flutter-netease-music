22732266e82bad78c9d433c3fe640f0e0d642ae5
boyan <yangbinyhbn@gmail.com>
2018-12-21 12:47:23 +0800

    add result verify for Loader

--- lib/part/part_stated_page.dart
+++ lib/part/part_stated_page.dart
@@ -5,19 +5,52 @@ import 'package:flutter/material.dart';
 typedef LoaderWidgetBuilder = Widget Function(
     BuildContext context, dynamic result);
 
+///build widget when loader load failed
+///result and msg might be null
+typedef LoaderFailedWidgetBuilder = Widget Function(
+    BuildContext context, dynamic result, String msg);
+
+///the result of function [TaskResultVerify]
+class VerifyValue {
+  VerifyValue.success(this.result);
+
+  VerifyValue.errorMsg(this.errorMsg) : assert(errorMsg != null);
+
+  dynamic result;
+  String errorMsg;
+
+  bool get isSuccess => errorMsg == null;
+}
+
+///to verify [Loader.loadTask] result is success
+typedef TaskResultVerify = VerifyValue Function(dynamic result);
+
+final TaskResultVerify _emptyVerify = (dynamic result) {
+  return VerifyValue.success(result);
+};
+
 class Loader extends StatefulWidget {
   const Loader(
       {Key key,
       @required this.loadTask,
       @required this.builder,
-      this.loadingBuilder})
-      : super(key: key);
+      this.resultVerify,
+      this.loadingBuilder,
+      this.failedWidgetBuilder})
+      : assert(loadTask != null),
+        assert(builder != null),
+        super(key: key);
 
   ///task to load
   ///returned future'data will send by [LoaderWidgetBuilder]
   final Future<dynamic> Function() loadTask;
 
   final LoaderWidgetBuilder builder;
+
+  final TaskResultVerify resultVerify;
+
+  ///if null, build a default error widget when load failed
+  final LoaderFailedWidgetBuilder failedWidgetBuilder;
 
   ///widget display when loading
   ///if null ,default to display a white background with a Circle Progress
@@ -31,11 +64,16 @@ class Loader extends StatefulWidget {
   State<StatefulWidget> createState() => LoaderState();
 }
 
+enum _LoaderState {
+  loading,
+  success,
+  failed,
+}
+
 class LoaderState extends State<Loader> {
-  /// 0 : loading
-  /// 1 : load success
-  /// 2 : load failed
-  int state = 0;
+  _LoaderState state = _LoaderState.loading;
+
+  String _errorMsg;
 
   CancelableOperation task;
 
@@ -52,17 +90,25 @@ class LoaderState extends State<Loader> {
   }
 
   void _loadData() {
-    state = 0;
+    state = _LoaderState.loading;
     task?.cancel();
     task = CancelableOperation.fromFuture(widget.loadTask())
       ..value.then((v) {
-        setState(() {
-          this.value = v;
-          state = 1;
-        });
+        var verify = (widget.resultVerify ?? _emptyVerify)(v);
+        if (verify.isSuccess) {
+          setState(() {
+            this.value = verify.result;
+            state = _LoaderState.success;
+          });
+        } else {
+          setState(() {
+            state = _LoaderState.failed;
+            _errorMsg = verify.errorMsg;
+          });
+        }
       }).catchError(() {
         setState(() {
-          state = 2;
+          state = _LoaderState.failed;
         });
       });
   }
@@ -75,9 +121,9 @@ class LoaderState extends State<Loader> {
 
   @override
   Widget build(BuildContext context) {
-    if (state == 1) {
+    if (state == _LoaderState.success) {
       return widget.builder(context, value);
-    } else if (state == 0) {
+    } else if (state == _LoaderState.loading) {
       return widget.loadingBuilder != null
           ? widget.loadingBuilder(context)
           : Container(
@@ -87,15 +133,39 @@ class LoaderState extends State<Loader> {
               ),
             );
     }
-    return Center(
-      child: RaisedButton(
-        onPressed: () {
-          setState(() {
-            _loadData();
-          });
-        },
-        child: Text("加载失败，点击重试"),
-      ),
-    );
+    return widget.failedWidgetBuilder != null
+        ? widget.failedWidgetBuilder(context, value, _errorMsg)
+        : Scaffold(
+            body: Container(
+              constraints: BoxConstraints.expand(),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  Spacer(),
+                  _errorMsg == null
+                      ? null
+                      : Padding(
+                          padding:
+                              EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+                          child: Text(
+                            _errorMsg,
+                            style: Theme.of(context).textTheme.body1,
+                          ),
+                        ),
+                  RaisedButton(
+                    color: Theme.of(context).primaryColor,
+                    textColor: Theme.of(context).primaryTextTheme.title.color,
+                    onPressed: () {
+                      setState(() {
+                        _loadData();
+                      });
+                    },
+                    child: Text("加载失败，点击重试"),
+                  ),
+                  Spacer(),
+                ]..removeWhere((v) => v == null),
+              ),
+            ),
+          );
   }
 }
