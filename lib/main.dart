09d68e2606d08ac1cc38a7b2c7824eadeec27596
boyan <yangbinyhbn@gmail.com>
2018-12-29 14:00:52 +0800

    add shadow for body text style

--- lib/main.dart
+++ lib/main.dart
@@ -14,17 +14,20 @@ class MyApp extends StatelessWidget {
           routes: routes,
           title: 'Quiet',
           theme: ThemeData(
-            // This is the theme of your application.
-            //
-            // Try running your application with "flutter run". You'll see the
-            // application has a blue toolbar. Then, without quitting the app, try
-            // changing the primarySwatch below to Colors.green and then invoke
-            // "hot reload" (press "r" in the console where you ran "flutter run",
-            // or simply save your changes to "hot reload" in a Flutter IDE).
-            // Notice that the counter didn't reset back to zero; the application
-            // is not restarted.
-            primarySwatch: Colors.green,
-          ),
+              // This is the theme of your application.
+              //
+              // Try running your application with "flutter run". You'll see the
+              // application has a blue toolbar. Then, without quitting the app, try
+              // changing the primarySwatch below to Colors.green and then invoke
+              // "hot reload" (press "r" in the console where you ran "flutter run",
+              // or simply save your changes to "hot reload" in a Flutter IDE).
+              // Notice that the counter didn't reset back to zero; the application
+              // is not restarted.
+              primarySwatch: Colors.green,
+              textTheme: TextTheme(
+                  body1: TextStyle(shadows: [
+                Shadow(offset: Offset(0.1, 0.1),blurRadius: 0.5, color: Colors.black87)
+              ]))),
         ),
       ),
     );
