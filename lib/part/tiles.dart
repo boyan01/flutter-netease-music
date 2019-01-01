ceb7ad5a0e81ee131878f79a1dd439f7300560cd
boyan <yangbinyhbn@gmail.com>
2019-01-01 19:49:17 +0800

    add artist detail page

---
+++ lib/part/tiles.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:quiet/repository/netease_image.dart';
+import 'package:quiet/pages/page_playlist_detail.dart';
+
+class AlbumTile extends StatelessWidget {
+  const AlbumTile({Key key, @required this.album, this.subtitle})
+      : assert(album != null),
+        super(key: key);
+
+  ///netease album json object
+  final Map album;
+
+  final String Function(Map album) subtitle;
+
+  String _defaultSubtitle(Map album) {
+    String date = DateFormat("y.M.d")
+        .format(DateTime.fromMillisecondsSinceEpoch(album["publishTime"]));
+    return "$date 歌曲 ${album["size"]}";
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    String subtitle = (this.subtitle ?? _defaultSubtitle)(album);
+    return InkWell(
+      onTap: () {
+        Navigator.of(context).push(MaterialPageRoute(builder: (context) {
+          return AlbumDetailPage(albumId: album["id"], album: album);
+        }));
+      },
+      child: Container(
+        height: 64,
+        child: Row(
+          children: <Widget>[
+            Padding(
+              padding: EdgeInsets.all(4),
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(4),
+                child: AspectRatio(
+                  aspectRatio: 1,
+                  child: Image(
+                      image: NeteaseImage(album["picUrl"]), fit: BoxFit.cover),
+                ),
+              ),
+            ),
+            Padding(padding: EdgeInsets.only(left: 4)),
+            Expanded(
+                child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Spacer(),
+                Text(album["name"], maxLines: 1),
+                Spacer(),
+                Text(subtitle,
+                    maxLines: 1, style: Theme.of(context).textTheme.caption),
+                Spacer(),
+                Divider(height: 0)
+              ],
+            ))
+          ],
+        ),
+      ),
+    );
+  }
+}
