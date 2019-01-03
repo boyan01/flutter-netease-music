f2c84d048c4fc3236a30f2ce71629050e8fbb987
boyan <yangbinyhbn@gmail.com>
2019-01-03 17:16:36 +0800

    添加歌单的收藏与取消收藏

--- lib/model/playlist_detail.dart
+++ lib/model/playlist_detail.dart
@@ -2,8 +2,16 @@ import 'package:quiet/model/model.dart';
 import 'package:quiet/repository/netease.dart';
 
 class PlaylistDetail {
-  PlaylistDetail(this.id, this.musicList, this.creator, this.name,
-      this.coverUrl, this.trackCount, this.description);
+  PlaylistDetail(
+      this.id,
+      this.musicList,
+      this.creator,
+      this.name,
+      this.coverUrl,
+      this.trackCount,
+      this.description,
+      this.subscribed,
+      this.subscribedCount);
 
   ///null when playlist not complete loaded
   final List<Music> musicList;
@@ -17,6 +25,10 @@ class PlaylistDetail {
   int trackCount;
 
   String description;
+
+  bool subscribed;
+
+  int subscribedCount;
 
   bool get loaded => musicList != null && musicList.length == trackCount;
 
@@ -38,6 +50,8 @@ class PlaylistDetail {
         playlist["name"],
         playlist["coverImgUrl"],
         playlist["trackCount"],
-        playlist["description"]);
+        playlist["description"],
+        playlist["subscribed"],
+        playlist["subscribedCount"]);
   }
 }
