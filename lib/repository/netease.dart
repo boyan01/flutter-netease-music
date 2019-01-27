73bebfbe24420f700b0085132a68fc6d74c233f3
boyan <yangbinyhbn@gmail.com>
2019-01-27 15:19:29 +0800

    add subscribe for mv

--- lib/repository/netease.dart
+++ lib/repository/netease.dart
@@ -439,6 +439,18 @@ class NeteaseRepository {
     return doRequest('https://music.163.com/weapi/mv/detail', {'id': mvId});
   }
 
+  ///调用此接口,可收藏 MV
+  Future<void> mvSubscribe(int mvId, bool subscribe) async {
+    final action = subscribe ? 'sub' : 'unsub';
+    final result = responseVerify(await doRequest(
+        'https://music.163.com/weapi/mv/$action',
+        {'mvId': mvId, 'mvIds': '["$mvId"]'}));
+    if (result.isSuccess) {
+      return;
+    }
+    throw result.errorMsg;
+  }
+
   //请求数据
   Future<Map<String, dynamic>> doRequest(String path, Map data,
       {EncryptType type = EncryptType.we,
