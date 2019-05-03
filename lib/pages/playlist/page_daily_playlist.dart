685c7a35b539fe4bdb8012767065c2d81660e595
boyan <yangbinyhbn@gmail.com>
2019-05-04 01:52:43 +0800

    feat: 完善每日推荐页面样式

--- lib/pages/playlist/page_daily_playlist.dart
+++ lib/pages/playlist/page_daily_playlist.dart
@@ -6,24 +6,13 @@ import 'package:quiet/part/part.dart';
 import 'package:quiet/repository/netease.dart';
 import 'package:url_launcher/url_launcher.dart';
 
-///每日推荐歌曲
+///每日推荐歌曲页面
+///NOTE：需要登陆
 class DailyPlaylistPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        leading: BackButton(),
-        titleSpacing: 0,
-        title: const Text("每日推荐"),
-        actions: <Widget>[
-          IconButton(
-              icon: Icon(Icons.help_outline),
-              onPressed: () {
-                launch("http://music.163.com/m/topic/19193112",
-                    forceWebView: true);
-              })
-        ],
-      ),
+      resizeToAvoidBottomInset: false,
       body: BoxWithBottomPlayerController(
         Loader<Map<String, Object>>(
             loadTask: () => neteaseRepository.recommendSongs(),
@@ -39,85 +28,88 @@ class DailyPlaylistPage extends StatelessWidget {
                   trailingBuilder: MusicList.defaultTrailingBuilder,
                   leadingBuilder: MusicList.coverLeadingBuilder,
                   onMusicTap: MusicList.defaultOnTap,
-                  child: ListView.builder(
-                      itemCount: list.length + 1,
-                      itemBuilder: (context, index) {
-                        if (index == 0) {
-                          return MusicListHeader(list.length);
-                        } else {
-                          return MusicTile(list[index - 1]);
-                        }
-                      }));
+                  child: _DailyMusicList());
             }),
       ),
     );
   }
 }
 
-///header of daily recommend list
-class _Header extends StatelessWidget {
+///数据加载成功后的整体页面
+///主要分为两部分：
+///1. head: 包括标题 header 和播放全部 header
+///2. content: 音乐列表
+class _DailyMusicList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-          image: DecorationImage(
-              fit: BoxFit.cover,
-              image: AssetImage("assets/daily_list_background.webp"))),
-      height: 150,
-      child: Container(
-        padding: EdgeInsets.only(left: 16),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            Spacer(),
-            _Calendar(),
-            Padding(padding: EdgeInsets.only(top: 12)),
-            Row(
-              children: <Widget>[
-                Icon(
-                  Icons.bubble_chart,
-                  color: Theme.of(context).primaryTextTheme.caption.color,
-                ),
-                Text(
-                  "自动生成，每天6:00更新",
-                  style: Theme.of(context).primaryTextTheme.caption,
-                )
-              ],
+    return NestedScrollView(
+      headerSliverBuilder: (context, innerBoxIsScrolled) {
+        return [
+          SliverAppBar(
+            title: Text('每日推荐'),
+            titleSpacing: 0,
+            forceElevated: innerBoxIsScrolled,
+            elevation: 0,
+            actions: <Widget>[
+              IconButton(
+                  icon: Icon(Icons.help_outline),
+                  onPressed: () {
+                    launch("http://music.163.com/m/topic/19193112",
+                        forceWebView: true);
+                  })
+            ],
+            flexibleSpace: Container(
+              padding: EdgeInsets.only(bottom: 50, top: kToolbarHeight),
+              child: SafeArea(child: _HeaderContent()),
             ),
-            Padding(padding: EdgeInsets.only(top: 12))
-          ],
-        ),
+            expandedHeight: 232 - MediaQuery.of(context).padding.top,
+            pinned: true,
+            bottom: MusicListHeader(MusicList.of(context).musics.length),
+          ),
+        ];
+      },
+      body: MediaQuery.removePadding(
+        removeTop: true,
+        context: context,
+        child: ListView.builder(
+            itemCount: MusicList.of(context).musics.length,
+            itemBuilder: (context, index) {
+              return MusicTile(MusicList.of(context).musics[index]);
+            }),
       ),
     );
   }
 }
 
-class _Calendar extends StatelessWidget {
+///每日推荐 Header 区域内容
+class _HeaderContent extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 64,
-      child: AspectRatio(
-        aspectRatio: 1,
-        child: Stack(
+    final date = DateTime.now();
+    final textTheme = Theme.of(context).primaryTextTheme;
+    return DefaultTextStyle(
+      maxLines: 1,
+      style: textTheme.body1.copyWith(fontWeight: FontWeight.bold),
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16),
+        child: Column(
+          mainAxisSize: MainAxisSize.max,
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
-            Icon(
-              Icons.calendar_today,
-              size: 64,
-              color: Theme.of(context).primaryIconTheme.color,
+            Spacer(),
+            Text.rich(TextSpan(children: [
+              TextSpan(
+                  text: date.day.toString().padLeft(2, '0'),
+                  style: TextStyle(fontSize: 23)),
+              TextSpan(text: ' / '),
+              TextSpan(text: date.month.toString().padLeft(2, '0')),
+            ])),
+            SizedBox(height: 4),
+            Text(
+              '根据你的音乐口味，为你推荐好音乐',
+              style: textTheme.caption,
             ),
-            Container(
-              child: Align(
-                alignment: Alignment(0, 0.5),
-                child: Text(
-                  DateTime.now().day.toString(),
-                  style: Theme.of(context)
-                      .primaryTextTheme
-                      .body1
-                      .copyWith(fontSize: 28, fontWeight: FontWeight.w400),
-                ),
-              ),
-            )
+            Spacer(),
           ],
         ),
       ),
