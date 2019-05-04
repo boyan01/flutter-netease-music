2fd55a6fb2e33dcf6398d9321c5f388221d513c0
boyan <yangbinyhbn@gmail.com>
2019-05-04 13:20:41 +0800

    test: add tester for PageNeedLogin

---
+++ test/pages/account/test_page_need_login.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
+import 'package:quiet/pages/account/account.dart';
+import 'package:quiet/pages/account/page_need_login.dart';
+import 'package:scoped_model/scoped_model.dart';
+
+import '../../repository/mock.dart';
+import '../../widget_test_context.dart';
+
+void main() {
+  testWidgets('test need login', (tester) async {
+    final loginModel = MockLoginState();
+    when(loginModel.isLogin).thenReturn(false);
+
+    await tester.pumpWidget(TestContext(
+      child: ScopedModel<UserAccount>(
+          model: loginModel,
+          child: PageNeedLogin(
+            builder: (context) => Container(),
+          )),
+    ));
+    expect(find.text('当前页面需要登陆'), findsOneWidget);
+  });
+
+  testWidgets('test has login', (tester) async {
+    final loginModel = MockLoginState();
+    when(loginModel.isLogin).thenReturn(true);
+
+    await tester.pumpWidget(TestContext(
+      child: ScopedModel<UserAccount>(
+          model: loginModel,
+          child: PageNeedLogin(
+            builder: (context) => Container(child: Text('已登录')),
+          )),
+    ));
+    expect(find.text('当前页面需要登陆'), findsNothing);
+    expect(find.text('已登录'), findsOneWidget);
+  });
+}
