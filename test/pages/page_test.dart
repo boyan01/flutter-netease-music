d3d6f8939c1d01267852251110e8292dc4bd1f15
boyan <yangbinyhbn@gmail.com>
2019-05-19 05:18:20 +0800

    test: add page test case

--- test/pages/page_test.dart
+++ test/pages/page_test.dart
@@ -1,10 +1,13 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/mockito.dart';
 import 'package:quiet/component/netease/netease.dart';
+import 'package:quiet/model/playlist_detail.dart';
 import 'package:quiet/pages/artists/page_artist_detail.dart';
 import 'package:quiet/pages/collection/api.dart';
 import 'package:quiet/pages/comments/page_comment.dart';
 import 'package:quiet/pages/playlist/music_list.dart';
+import 'package:quiet/pages/record/page_record.dart';
 import 'package:quiet/part/part.dart';
 import 'package:quiet/repository/netease.dart';
 
@@ -12,12 +15,17 @@ import '../repository/json/artist_detail.dart';
 import '../repository/json/collection.dart';
 import '../repository/json/comment.dart';
 import '../repository/json/leader_board.dart';
+import '../repository/json/playlist_detail.dart';
+import '../repository/json/record.dart';
 import '../repository/mock.dart';
 import '../widget_test_context.dart';
 import 'my_collection_page_test.dart';
 
 void main() {
   neteaseRepository = MockNeteaseRepository();
+  neteaseLocalData = MockNeteaseLocalData();
+  when(neteaseLocalData.get(any)).thenAnswer((_) => Future.value(null));
+
   enableCache = false;
 
   testWidgets('aritst page', (tester) async {
@@ -76,5 +84,64 @@ void main() {
 
   testWidgets('main page', (tester) async {
     await tester.pumpWidget(TestContext(child: MainPage()));
+
+    await tester.pump(const Duration(milliseconds: 100));
+
+    expect(find.byIcon(Icons.music_note), findsWidgets);
+    expect(find.text('当前未登录，点击登录!'), findsWidgets);
+  });
+
+  testWidgets('playlist detail page', (tester) async {
+    when(neteaseRepository.playlistDetail(84687600)).thenAnswer((_) =>
+        Future.value(
+            Result.value(PlaylistDetail.fromJson(playlist['playlist']))));
+    await tester.pumpWidget(TestContext(child: PlaylistDetailPage(84687600)));
+
+    await tester.pump(const Duration(milliseconds: 100));
+
+    expect(find.text('1'), findsWidgets);
+    expect(find.text('我要夏天的一半喜欢的音乐'), findsOneWidget);
+    expect(find.text('太阳系DISCO（Cover miku）'), findsOneWidget);
+    expect(find.byType(MusicTile), findsWidgets);
+  });
+
+  testWidgets('dialy playlist page', (tester) async {
+    when(neteaseRepository.recommendSongs())
+        .thenAnswer((_) => Future.value(Result.value(recommend)));
+    final account = MockLoginState();
+    when(account.isLogin).thenReturn(true);
+    await tester.pumpWidget(TestContext(
+        child: ScopedModel<UserAccount>(
+            model: account, child: DailyPlaylistPage())));
+    await tester.pump(const Duration(milliseconds: 100));
+
+    expect(find.text('每日推荐'), findsWidgets);
+    expect(find.byType(MusicTile), findsWidgets);
+    expect(find.text('倒数'), findsOneWidget);
+    expect(find.text('像我这样的人'), findsOneWidget);
+  });
+
+  testWidgets('album detail page', (tester) async {
+    when(neteaseRepository.albumDetail(77430187))
+        .thenAnswer((_) => Future.value(Result.value(album)));
+    await tester
+        .pumpWidget(TestContext(child: AlbumDetailPage(albumId: 77430187)));
+    await tester.pump(const Duration(milliseconds: 100));
+
+    expect(find.text('427'), findsWidgets);
+    expect(find.text('2019翻唱合集（0407更新）'), findsWidgets);
+    expect(find.text('八足的末日物语（Cover：JUSF周存ft.洛天依）'), findsOneWidget);
+    expect(find.byType(MusicTile), findsWidgets);
+  });
+
+  testWidgets('record page', (tester) async {
+    when(neteaseRepository.getRecord(any, any))
+        .thenAnswer((_) => Future.value(Result.value(record)));
+    await tester.pumpWidget(TestContext(child: RecordPage(uid: 12)));
+    await tester.pump(const Duration(milliseconds: 100));
+
+    expect(find.byType(MusicTile), findsWidgets);
+    expect(find.text('藏'), findsWidgets);
+    expect(find.text('花儿纳吉（Cover 洛天依）'), findsOneWidget);
   });
 }
--- test/repository/mock.dart
+++ test/repository/mock.dart
@@ -1,3 +1,4 @@
+import 'package:quiet/component/netease/netease.dart';
 import 'package:quiet/part/part.dart';
 import 'package:quiet/repository/netease.dart';
 import 'package:mockito/mockito.dart';
@@ -7,3 +8,7 @@ class MockNeteaseRepository extends Mock implements NeteaseRepository {}
 class MockNeteaseLocalData extends Mock implements NeteaseLocalData {}
 
 class MockLoginState extends Mock implements UserAccount {}
+
+class MockLikedSongList extends Mock implements LikedSongList {}
+
+class MockCounter extends Mock implements Counter {}
