c47cae73031a99681bf4fb6e82dc634aae3e864c
boyan <yangbinyhbn@gmail.com>
2019-05-24 18:43:23 +0800

    抽出网易云音乐风格的TabBar

--- lib/material/tabs.dart
+++ lib/material/tabs.dart
@@ -6,3 +6,29 @@ class PrimaryTabIndicator extends UnderlineTabIndicator {
             insets: const EdgeInsets.only(bottom: 4),
             borderSide: BorderSide(color: color, width: 2.0));
 }
+
+///网易云音乐风格的TabBar
+class RoundedTabBar extends StatelessWidget implements PreferredSizeWidget {
+  final List<Widget> tabs;
+
+  const RoundedTabBar({Key key, @required this.tabs}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ClipRRect(
+      borderRadius: BorderRadius.vertical(top: Radius.circular(15)),
+      child: Material(
+        color: Theme.of(context).scaffoldBackgroundColor,
+        child: TabBar(
+            indicator:
+                PrimaryTabIndicator(color: Theme.of(context).primaryColor),
+            indicatorSize: TabBarIndicatorSize.label,
+            labelColor: Colors.black87,
+            tabs: tabs),
+      ),
+    );
+  }
+
+  @override
+  Size get preferredSize => Size.fromHeight(50);
+}
