f7e07e0e06bd255fd93f39d65c551275011572e9
boyan <yangbinyhbn@gmail.com>
2019-06-11 15:43:24 +0800

    pref: 移动copyright_overlay逻辑到 Settings

--- lib/material/app.dart
+++ lib/material/app.dart
@@ -1,62 +1,22 @@
-import 'package:flutter/material.dart';
 import 'dart:math' as math;
-import 'package:shared_preferences/shared_preferences.dart';
+
+import 'package:flutter/material.dart';
+import 'package:quiet/component/global/settings.dart';
 
 ///版权说明浮层
-class CopyRightOverlay extends StatefulWidget {
+class CopyRightOverlay extends StatelessWidget {
   final Widget child;
 
-  const CopyRightOverlay({Key key, this.child}) : super(key: key);
-
-  static void setDismiss(BuildContext context, bool dismiss) {
-    _CopyRightOverlayState state = context
-        .ancestorStateOfType(const TypeMatcher<_CopyRightOverlayState>());
-    state._setDismissed(dismiss);
-  }
-
-  static bool isShouldDismiss(BuildContext context) {
-    _CopyRightOverlayState state = context
-        .ancestorStateOfType(const TypeMatcher<_CopyRightOverlayState>());
-    return state.dismiss;
-  }
-
-  @override
-  _CopyRightOverlayState createState() => _CopyRightOverlayState();
-}
-
-class _CopyRightOverlayState extends State<CopyRightOverlay> {
   final _painter = _CopyrightPainter();
 
-  static final _keyDismiss = 'dissmiss_copy_right_overlay';
-
-  bool dismiss = true;
-
-  @override
-  void initState() {
-    super.initState();
-    SharedPreferences.getInstance().then((preferences) {
-      setState(() {
-        dismiss = preferences.getBool(_keyDismiss) ?? false;
-      });
-    });
-  }
-
-  void _setDismissed(bool dismiss) {
-    if (this.dismiss == dismiss) return;
-    setState(() {
-      this.dismiss = dismiss;
-      SharedPreferences.getInstance().then((preferences) {
-        preferences.setBool(_keyDismiss, dismiss);
-      });
-    });
-  }
+  CopyRightOverlay({Key key, this.child}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    debugPrint("build with painter : $dismiss");
     return CustomPaint(
-      foregroundPainter: dismiss ? null : _painter,
-      child: widget.child,
+      foregroundPainter:
+          Settings.of(context).showCopyrightOverlay ? null : _painter,
+      child: child,
     );
   }
 }
