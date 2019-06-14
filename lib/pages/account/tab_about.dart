42062dded092bc5261c850098f17d148be2beda8
boyan <yangbinyhbn@gmail.com>
2019-06-14 19:32:52 +0800

    feat: add tabs for user_detail_page

---
+++ lib/pages/account/tab_about.dart
@@ -0,0 +1,18 @@
+part of 'page_user_detail.dart';
+
+class TabAbout extends StatefulWidget {
+  @override
+  _TabAboutState createState() => _TabAboutState();
+}
+
+class _TabAboutState extends State<TabAbout>
+    with AutomaticKeepAliveClientMixin {
+  @override
+  bool get wantKeepAlive => true;
+
+  @override
+  Widget build(BuildContext context) {
+    super.build(context);
+    return Container();
+  }
+}
---
+++ lib/pages/account/tab_music.dart
@@ -0,0 +1,18 @@
+part of 'page_user_detail.dart';
+
+class TabMusic extends StatefulWidget {
+  @override
+  _TabMusicState createState() => _TabMusicState();
+}
+
+class _TabMusicState extends State<TabMusic>
+    with AutomaticKeepAliveClientMixin {
+  @override
+  bool get wantKeepAlive => true;
+
+  @override
+  Widget build(BuildContext context) {
+    super.build(context);
+    return Container();
+  }
+}
