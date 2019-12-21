cc8f33f578e8a80b09c2274d4739ab1212c32104
伯言 <yangbinyhbn@gmail.com>
2019-12-22 01:44:39 +0800

    avoid Tree Shaking in release mode

--- lib/main.dart
+++ lib/main.dart
@@ -35,6 +35,7 @@ void main() {
 
 /// The entry of dart background service
 /// NOTE: this method will be invoked by native (Android/iOS)
+@pragma('vm:entry-point') // avoid Tree Shaking
 void playerBackgroundService() {
   WidgetsFlutterBinding.ensureInitialized();
   // 获取播放地址需要使用云音乐 API, 所以需要为此 isolate 初始化一个 repository.
