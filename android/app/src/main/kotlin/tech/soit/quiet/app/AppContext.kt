556f9bc25cc72de5e7c8d72a08a72bc2a828083e
boyan <yangbinyhbn@gmail.com>
2020-03-06 22:27:57 +0800

    fix: android build
    Signed-off-by: boyan <yangbinyhbn@gmail.com>

--- android/app/src/main/kotlin/tech/soit/quiet/app/AppContext.kt
+++ android/app/src/main/kotlin/tech/soit/quiet/app/AppContext.kt
@@ -3,16 +3,11 @@ package tech.soit.quiet.app
 import io.flutter.app.FlutterApplication
 import io.flutter.plugins.pathprovider.PathProviderPlugin
 import io.flutter.plugins.sharedpreferences.SharedPreferencesPlugin
-import tech.soit.quiet.MusicPlayerServicePlugin
 
 @Suppress("unused")
 class AppContext : FlutterApplication() {
 
     override fun onCreate() {
         super.onCreate()
-        MusicPlayerServicePlugin.setOnRegisterCallback { engine ->
-            engine.plugins.add(PathProviderPlugin())
-            engine.plugins.add(SharedPreferencesPlugin())
-        }
     }
 }
