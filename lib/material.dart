521e3568dbbb2caaeb0a48c9bcdf09e3691cfc58
boyan <yangbinyhbn@gmail.com>
2020-09-19 12:38:40 +0800

    dev
    Signed-off-by: boyan <yangbinyhbn@gmail.com>

--- lib/material.dart
+++ lib/material.dart
@@ -1,3 +1,5 @@
+library meterial;
+
 export 'material/app.dart';
 export 'material/button.dart';
 export 'material/dialogs.dart';
@@ -5,7 +7,7 @@ export 'material/dividers.dart';
 export 'material/flexible_app_bar.dart';
 export 'material/hero.dart';
 export 'material/images.dart';
+export 'material/landscape.dart';
 export 'material/player.dart';
 export 'material/tabs.dart';
 export 'material/tiles.dart';
-export 'material/landscape.dart';
---
+++ lib/src/scaffold/keyboard.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class FastJumpEmitter extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
