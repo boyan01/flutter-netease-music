fb7daa33f6fa6dc997de4b3163d5589829ac2e8e
伯言 <yangbinyhbn@gmail.com>
2020-09-20 22:23:26 +0800

    add about page

--- lib/pages/setting/page_setting.dart
+++ lib/pages/setting/page_setting.dart
@@ -1,13 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:quiet/component.dart';
 import 'package:quiet/component/global/settings.dart';
 import 'package:quiet/component/route.dart';
-import 'package:quiet/component.dart';
 
 import 'material.dart';
 
 export 'setting_theme_page.dart';
 
-///App 设置页面
+/// App 设置页面
 class SettingPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -28,6 +28,25 @@ class SettingPage extends StatelessWidget {
               _CopyRightCheckBox(),
             ],
           ),
+          SettingGroup(
+            title: "关于",
+            children: [
+              ListTile(
+                title: Text("关于我们"),
+                onTap: () {
+                  showDialog(
+                      context: context,
+                      builder: (context) {
+                        return AboutDialog(
+                          applicationIcon: Image.asset("assets/ic_launcher_round.png"),
+                          applicationVersion: "0.3-alpha",
+                          applicationLegalese: "此应用仅供学习交流使用，请勿用于任何商业用途。",
+                        );
+                      });
+                },
+              )
+            ],
+          )
         ],
       ),
     );
