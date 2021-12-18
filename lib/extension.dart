f397d08476bca38e52300c2006af28bc0886c0c5
boyan <17426470+boyan01@users.noreply.github.com>
2021-12-18 23:56:56 +0800

    fix android build

--- lib/extension.dart
+++ lib/extension.dart
@@ -2,7 +2,6 @@ export 'package:collection/collection.dart';
 export 'package:provider/provider.dart' show ReadContext, WatchContext;
 
 export 'extension/formats.dart';
-export 'extension/lists.dart';
 export 'extension/numbers.dart';
 
 void importExtension() {}
