228b09a5fc2ed74a865fcb3150a3f3fd0e13c6b1
boyan <17426470+boyan01@users.noreply.github.com>
2021-12-19 22:58:09 +0800

    add local cache for playlistDetailProvider.

--- lib/providers/playlist_detail_provider.dart
+++ lib/providers/playlist_detail_provider.dart
@@ -1,9 +1,24 @@
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:quiet/repository.dart';
 
-final playlistDetailProvider = FutureProvider.family<PlaylistDetail, int>(
-  (ref, playlistId) async {
+final playlistDetailProvider = StreamProvider.family<PlaylistDetail, int>(
+  (ref, playlistId) async* {
+    final local = await neteaseLocalData.getPlaylistDetail(playlistId);
+    if (local != null) {
+      yield local;
+    }
     final ret = await neteaseRepository!.playlistDetail(playlistId);
-    return ret.asFuture;
+    var detail = await ret.asFuture;
+
+    if (local != null && detail.trackUpdateTime == local.trackUpdateTime) {
+      detail = detail.copyWith(tracks: local.tracks);
+    } else if (detail.tracks.length != detail.trackCount) {
+      final musics = await neteaseRepository!.songDetails(detail.trackIds);
+      if (musics.isValue) {
+        detail = detail.copyWith(tracks: musics.asValue!.value);
+      }
+    }
+    neteaseLocalData.updatePlaylistDetail(detail);
+    yield detail;
   },
 );
