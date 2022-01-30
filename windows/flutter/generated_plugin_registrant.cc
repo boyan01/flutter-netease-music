77d75a81c2c6fdc60eebab888d549840d6e3bfcf
boyan01 <17426470+boyan01@users.noreply.github.com>
2022-01-30 10:58:34 +0800

    update deps.

--- windows/flutter/generated_plugin_registrant.cc
+++ windows/flutter/generated_plugin_registrant.cc
@@ -7,6 +7,7 @@
 #include "generated_plugin_registrant.h"
 
 #include <dart_vlc/dart_vlc_plugin.h>
+#include <desktop_drop/desktop_drop_plugin.h>
 #include <system_clock/system_clock_plugin.h>
 #include <url_launcher_windows/url_launcher_windows.h>
 #include <window_manager/window_manager_plugin.h>
@@ -15,6 +16,8 @@
 void RegisterPlugins(flutter::PluginRegistry* registry) {
   DartVlcPluginRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("DartVlcPlugin"));
+  DesktopDropPluginRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("DesktopDropPlugin"));
   SystemClockPluginRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("SystemClockPlugin"));
   UrlLauncherWindowsRegisterWithRegistrar(
