27728f7516f44585a03531e8229c2da9f410043c
boyan01 <17426470+boyan01@users.noreply.github.com>
2022-07-24 15:00:39 +0800

    fix system_fonts.dart traling comma.

--- lib/utils/system/system_fonts.dart
+++ lib/utils/system/system_fonts.dart
@@ -32,11 +32,14 @@ Future<void> loadFallbackFonts() async {
     final matchedResult = Process.runSync('fc-match', ['-f', '%{family}']);
     if (matchedResult.exitCode != 0) {
       debugPrint(
-          'failed to get best match font family. error: ${matchedResult.stderr}');
+        'failed to get best match font family. error: ${matchedResult.stderr}',
+      );
       return;
     }
-    final result = Process.runSync('fc-list',
-        ['-f', '%{family}:%{file}\n', matchedResult.stdout as String]);
+    final result = Process.runSync(
+      'fc-list',
+      ['-f', '%{family}:%{file}\n', matchedResult.stdout as String],
+    );
     final lines = const LineSplitter().convert(result.stdout as String);
     String? fontFamily;
     final fontPaths = <String>[];
@@ -44,8 +47,10 @@ Future<void> loadFallbackFonts() async {
     for (final line in lines) {
       // font config "family:file"
       final fontConfig = line.split(':');
-      assert(fontConfig.length == 2,
-          'font config do not match required format. $fontConfig');
+      assert(
+        fontConfig.length == 2,
+        'font config do not match required format. $fontConfig',
+      );
       if (fontFamily == null) {
         fontFamily = fontConfig.first;
         fontPaths.add(fontConfig[1]);
@@ -100,34 +105,64 @@ extension _TextTheme on TextTheme {
   TextTheme applyFonts(String? fontFamily, List<String>? fontFamilyFallback) =>
       copyWith(
         displayLarge: displayLarge?.copyWith(
-            fontFamily: fontFamily, fontFamilyFallback: fontFamilyFallback),
+          fontFamily: fontFamily,
+          fontFamilyFallback: fontFamilyFallback,
+        ),
         displayMedium: displayMedium?.copyWith(
-            fontFamily: fontFamily, fontFamilyFallback: fontFamilyFallback),
+          fontFamily: fontFamily,
+          fontFamilyFallback: fontFamilyFallback,
+        ),
         displaySmall: displaySmall?.copyWith(
-            fontFamily: fontFamily, fontFamilyFallback: fontFamilyFallback),
+          fontFamily: fontFamily,
+          fontFamilyFallback: fontFamilyFallback,
+        ),
         headlineLarge: headlineLarge?.copyWith(
-            fontFamily: fontFamily, fontFamilyFallback: fontFamilyFallback),
+          fontFamily: fontFamily,
+          fontFamilyFallback: fontFamilyFallback,
+        ),
         headlineMedium: headlineMedium?.copyWith(
-            fontFamily: fontFamily, fontFamilyFallback: fontFamilyFallback),
+          fontFamily: fontFamily,
+          fontFamilyFallback: fontFamilyFallback,
+        ),
         headlineSmall: headlineSmall?.copyWith(
-            fontFamily: fontFamily, fontFamilyFallback: fontFamilyFallback),
+          fontFamily: fontFamily,
+          fontFamilyFallback: fontFamilyFallback,
+        ),
         titleLarge: titleLarge?.copyWith(
-            fontFamily: fontFamily, fontFamilyFallback: fontFamilyFallback),
+          fontFamily: fontFamily,
+          fontFamilyFallback: fontFamilyFallback,
+        ),
         titleMedium: titleMedium?.copyWith(
-            fontFamily: fontFamily, fontFamilyFallback: fontFamilyFallback),
+          fontFamily: fontFamily,
+          fontFamilyFallback: fontFamilyFallback,
+        ),
         titleSmall: titleSmall?.copyWith(
-            fontFamily: fontFamily, fontFamilyFallback: fontFamilyFallback),
+          fontFamily: fontFamily,
+          fontFamilyFallback: fontFamilyFallback,
+        ),
         bodyLarge: bodyLarge?.copyWith(
-            fontFamily: fontFamily, fontFamilyFallback: fontFamilyFallback),
+          fontFamily: fontFamily,
+          fontFamilyFallback: fontFamilyFallback,
+        ),
         bodyMedium: bodyMedium?.copyWith(
-            fontFamily: fontFamily, fontFamilyFallback: fontFamilyFallback),
+          fontFamily: fontFamily,
+          fontFamilyFallback: fontFamilyFallback,
+        ),
         bodySmall: bodySmall?.copyWith(
-            fontFamily: fontFamily, fontFamilyFallback: fontFamilyFallback),
+          fontFamily: fontFamily,
+          fontFamilyFallback: fontFamilyFallback,
+        ),
         labelLarge: labelLarge?.copyWith(
-            fontFamily: fontFamily, fontFamilyFallback: fontFamilyFallback),
+          fontFamily: fontFamily,
+          fontFamilyFallback: fontFamilyFallback,
+        ),
         labelMedium: labelMedium?.copyWith(
-            fontFamily: fontFamily, fontFamilyFallback: fontFamilyFallback),
+          fontFamily: fontFamily,
+          fontFamilyFallback: fontFamilyFallback,
+        ),
         labelSmall: labelSmall?.copyWith(
-            fontFamily: fontFamily, fontFamilyFallback: fontFamilyFallback),
+          fontFamily: fontFamily,
+          fontFamilyFallback: fontFamilyFallback,
+        ),
       );
 }
