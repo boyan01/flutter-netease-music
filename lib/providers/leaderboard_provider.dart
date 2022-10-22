75c1d34ca05f51abf77ee1bafcff969231eb8da3
boyan <17426470+boyan01@users.noreply.github.com>
2022-10-22 18:20:19 +0800

    fix format

--- lib/providers/leaderboard_provider.dart
+++ lib/providers/leaderboard_provider.dart
@@ -3,7 +3,8 @@ import 'package:netease_api/netease_api.dart';
 
 import 'repository_provider.dart';
 
-final leaderboardProvider = FutureProvider.autoDispose<TopListDetail>((ref) async {
+final leaderboardProvider =
+    FutureProvider.autoDispose<TopListDetail>((ref) async {
   final repository = ref.read(neteaseRepositoryProvider);
   final topList = await repository.topListDetail();
   return topList.asFuture;
