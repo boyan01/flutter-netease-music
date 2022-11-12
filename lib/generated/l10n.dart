c0b5fb6cd32cb44a7b86ee3fae2ce79d0a70cee2
boyan <17426470+boyan01@users.noreply.github.com>
2022-11-12 08:57:05 +0800

    roll music_player version

--- lib/generated/l10n.dart
+++ lib/generated/l10n.dart
@@ -1256,10 +1256,8 @@ class AppLocalizationDelegate extends LocalizationsDelegate<S> {
 
   @override
   bool isSupported(Locale locale) => _isSupported(locale);
-
   @override
   Future<S> load(Locale locale) => S.load(locale);
-
   @override
   bool shouldReload(AppLocalizationDelegate old) => false;
 
