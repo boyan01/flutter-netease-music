d7a9f381cf25435b1d8b72eb64e4bdb210069172
bin <17426470+boyan01@users.noreply.github.com>
2023-02-26 11:24:16 +0800

    upgrade pubspec flutter version

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -7,9 +7,9 @@ import Foundation
 
 import desktop_drop
 import lychee_player
-import path_provider_macos
+import path_provider_foundation
 import screen_retriever
-import shared_preferences_macos
+import shared_preferences_foundation
 import url_launcher_macos
 import window_manager
 
