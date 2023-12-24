fd870d71887d17f05da1c4830d13bdc92ba30af0
bin <17426470+boyan01@users.noreply.github.com>
2023-12-24 09:28:02 +0800

    fix android build

--- android/app/src/main/kotlin/tech/soit/quiet/app/AppContext.kt
+++ android/app/src/main/kotlin/tech/soit/quiet/app/AppContext.kt
@@ -1,8 +1,6 @@
 package tech.soit.quiet.app
 
 import io.flutter.app.FlutterApplication
-import io.flutter.plugins.pathprovider.PathProviderPlugin
-import io.flutter.plugins.sharedpreferences.SharedPreferencesPlugin
 
 @Suppress("unused")
 class AppContext : FlutterApplication() {
